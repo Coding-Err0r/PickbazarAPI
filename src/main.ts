7508b37d503948ecde4c714131c55c7bc9ab5b67
Rhine <rhine.cse@gmail.com>
2023-09-03 15:55:07 +0600

    ✅  DOC:  Initilized

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Pick Bazar')
+    .setDescription('Pick Bazar API')
+    .setVersion('1.0')
+    .addTag('pickbazar')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+  console.log(`Swagger is running on: ${await app.getUrl()}/docs/`);
+  console.log(`Application is running on: ${await app.getUrl()}/api/`);
+}
+bootstrap();
